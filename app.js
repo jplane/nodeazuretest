a9626063e14bcc5a29a5daba39ee21933de9d642
Josh Lane <jplane@gmail.com>
2016-03-22 09:11:25 -0400

    updated port number

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ app.get('/', function (req, res) {
   res.send(msg());
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
+var port = process.env.PORT || 3000;
+
+app.listen(port, function () {
+  console.log('Example app listening on port ' + port);
 });
